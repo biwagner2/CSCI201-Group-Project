b5c722d0fcc7718c0f8e9cf81a99faea6ef870c6
Brian Wagner <biwagner@usc.edu>
2023-11-26 22:28:04 -0800

    Most up to date project. No longer need to use script.

---
+++ CSCI201_GroupProj-1/src/main/java/com/CSCI201/StudySC/Repository/StudyGroupMembersRepository.java
@@ -0,0 +1,9 @@
+package com.CSCI201.StudySC.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.CSCI201.StudySC.model.StudyGroupMembers;
+
+public interface StudyGroupMembersRepository extends JpaRepository<StudyGroupMembers, Integer>{
+
+}
