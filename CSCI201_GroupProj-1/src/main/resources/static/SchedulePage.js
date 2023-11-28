b758a4a92f63691fe03791bb4085efd8eb7aad15
Zhuoyan Tao <terryt@usc.edu>
2023-11-27 23:15:13 -0800

    The entire Javascript to solve undefined in displaying

--- CSCI201_GroupProj-1/src/main/resources/static/SchedulePage.js
+++ CSCI201_GroupProj-1/src/main/resources/static/SchedulePage.js
@@ -1,41 +1,16 @@
-
-
-document.addEventListener('DOMContentLoaded', function () {
-    // Fetch study groups from the servlet
-    fetch('SchedulePageServlet')
-    
-        .then(response => response.json())
-        .then(data => {
-            // Populate the courseContainer div with study group information
-            populateCourseContainer(data);
-        })
-        .catch(error => {
-            console.error('Error fetching data:', error);
-        });
-});
-  
-
 function populateCourseContainer(studyGroups) {
-    // Get the existing courses container
     var courseContainer = document.querySelector(".courseContainer");
-
-    // Clear existing content in the courseContainer
     courseContainer.innerHTML = '';
 
-    // Organize study groups by coursename
     const groupedStudyGroups = groupByCoursename(studyGroups);
 
-    // Iterate through each coursename and its study groups
     groupedStudyGroups.forEach((groups, coursename) => {
-        // Create a course div
         var courseDiv = document.createElement("div");
         courseDiv.classList.add("course");
 
-        // Create course title div
         var courseTitleDiv = document.createElement("div");
         courseTitleDiv.classList.add("courseTitle");
 
-        // Generate a random background color for the h2 (similar to the original code)
         var randomOpacity = Math.random() * 0.3 + 0.2;
         var randomBgColor = 'rgba(' +
             Math.floor(Math.random() * 256) + ',' +
@@ -43,52 +18,53 @@ function populateCourseContainer(studyGroups) {
             Math.floor(Math.random() * 256) + ',' +
             randomOpacity + ')';
 
-        // Create an h2 element with a random background color and append it to the courseTitle div
         var heading = document.createElement("h2");
-        heading.textContent = coursename; // Set the coursename as the title
+        heading.textContent = coursename;
         heading.style.backgroundColor = randomBgColor;
         courseTitleDiv.appendChild(heading);
 
         courseDiv.appendChild(courseTitleDiv);
 
-        // Create label div
         var labelDiv = document.createElement("div");
         labelDiv.classList.add("label");
 
-        ["Status", "Location", "Time", "Size"].forEach(function (labelText) {
-            var label = document.createElement("a");
+        ["Status", "Location", "Time", "Size"].forEach(function(labelText) {
+            var label = document.createElement("span");
             label.textContent = labelText;
             labelDiv.appendChild(label);
         });
 
         courseDiv.appendChild(labelDiv);
 
-        // Create details div
         var detailsDiv = document.createElement("div");
         detailsDiv.classList.add("details");
 
-        // Iterate through study groups for this coursename and display information
         groups.forEach(group => {
             var groupDetailsDiv = document.createElement("div");
-            groupDetailsDiv.classList.add("details");
+            groupDetailsDiv.classList.add("groupDetails");
 
-            // Customize the details content based on study group information
             groupDetailsDiv.innerHTML = `
-                <p>${group.courseName} | ${group.startTime} - ${group.endTime} | Size: ${group.size}</p>
+                <p>Status | ${group.location} - ${group.meetingDate} ${group.meetingTimeStart} | Size: ${group.capacity}</p>
             `;
 
-            var joinBtn = document.createElement("div");
+            var joinBtn = document.createElement("button");
             joinBtn.classList.add("join-btn");
             joinBtn.textContent = "Join";
+            joinBtn.addEventListener('click', function() {
+                let email = getCookie('userEmail');
+                email = "testemail@usc.edu";
+                if (email) {
+                    joinStudyGroup(group.groupId, email);
+                } else {
+                    console.error('Email not found in cookie');
+                }
+            });
 
             groupDetailsDiv.appendChild(joinBtn);
             detailsDiv.appendChild(groupDetailsDiv);
         });
 
-        // Append the details div to the course div
         courseDiv.appendChild(detailsDiv);
-
-        // Append the course div to the existing courses container
         courseContainer.appendChild(courseDiv);
     });
 }
@@ -97,7 +73,7 @@ function groupByCoursename(studyGroups) {
     const groupedStudyGroups = new Map();
 
     studyGroups.forEach(group => {
-        const coursename = group.courseName;
+        const coursename = group.coursename;
 
         if (!groupedStudyGroups.has(coursename)) {
             groupedStudyGroups.set(coursename, []);
@@ -108,3 +84,62 @@ function groupByCoursename(studyGroups) {
 
     return groupedStudyGroups;
 }
+
+function getCookie(name) {
+    let cookieArray = document.cookie.split('; ');
+    let cookie = cookieArray.find(row => row.startsWith(name + '='));
+    return cookie ? cookie.split('=')[1] : null;
+}
+
+function joinStudyGroup(groupId, email) {
+    var studyGroupMembers = {
+        studyGroup: {
+            id: groupId
+        },
+        user: {
+            email: email
+        }
+    };
+
+    makeFetchRequest(studyGroupMembers);
+}
+
+function makeFetchRequest(studyGroupMembers) {
+    fetch('/addToStudyMembers', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(studyGroupMembers),
+    })
+    .then(response => {
+        if (!response.ok) {
+            console.error('Server responded with status:', response.status);
+            throw new Error('Network response was not ok');
+        }
+        return response.text();
+    })
+    .then(text => {
+        try {
+            const data = JSON.parse(text);
+            console.log('JSON data:', data);
+        } catch (e) {
+            console.error('Error parsing JSON:', e);
+            console.log('Original text:', text);
+        }
+    })
+    .catch((error) => {
+        console.error('Fetch error:', error);
+    });
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    fetch('SchedulePageServlet')
+        .then(response => response.json())
+        .then(data => {
+            populateCourseContainer(data);
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
+        });
+});
